c3fbc6ed68deeb594947b3d825f7034f0643d2da
Victor <32575098+752377941@users.noreply.github.com>
2020-12-25 14:58:59 +0800

    Update train_tacotron.py
    fix the style_wav from the config of gst_style_input

--- TTS/bin/train_tacotron.py
+++ TTS/bin/train_tacotron.py
@@ -447,14 +447,13 @@ def evaluate(model, criterion, ap, global_step, epoch, speaker_mapping=None):
         print(" | > Synthesizing test sentences")
         speaker_id = 0 if c.use_speaker_embedding else None
         speaker_embedding = speaker_mapping[list(speaker_mapping.keys())[randrange(len(speaker_mapping)-1)]]['embedding'] if c.use_external_speaker_embedding_file and c.use_speaker_embedding else None
-        style_wav = c.get("gst_style_input")
+        style_wav = c.gst.get("gst_style_input")
         if style_wav is None and c.use_gst:
             # inicialize GST with zero dict.
             style_wav = {}
             print("WARNING: You don't provided a gst style wav, for this reason we use a zero tensor!")
             for i in range(c.gst['gst_style_tokens']):
                 style_wav[str(i)] = 0
-        style_wav = c.get("gst_style_input")
         for idx, test_sentence in enumerate(test_sentences):
             try:
                 wav, alignment, decoder_output, postnet_output, stop_tokens, _ = synthesis(
